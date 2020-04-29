c381f2d37ce6cf62a1f58a343874e2a6f2b75f72
Abhinav KM <abhinavkm184@gmail.com>
2020-04-29 13:10:10 +0000

    pull

---
+++ client/build/precache-manifest.5099310e9266a2f5f162c77f6843ab61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fda34c009a8fa585102ab1a3d00f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a57b973dfd4cc97cd0d0",
+    "url": "/static/css/main.2fbdbe93.chunk.css"
+  },
+  {
+    "revision": "19bfb61ae7c9af0b1966",
+    "url": "/static/js/2.ccb107ab.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ccb107ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57b973dfd4cc97cd0d0",
+    "url": "/static/js/main.ff844492.chunk.js"
+  },
+  {
+    "revision": "8f97e2fd537d0845142f",
+    "url": "/static/js/runtime-main.ec7aa697.js"
+  }
+]);
