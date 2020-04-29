f90c47ac0f7a7e1341bec2ccc08aadf820739676
ben-jnr <bichubenkuruvilla@gmail.com>
2020-04-29 15:01:17 +0530

    Added Village names in alphabetical order

--- client/src/Components/Institution/ConstituencyAddForm.js
+++ client/src/Components/Institution/ConstituencyAddForm.js
@@ -7,19 +7,19 @@ function ConstituencyForm(props) {
                 <label for="constituencyAdd">Constituency</label>
                 <select class="custom-select" name="constituency" id="constituencyAdd" size="1" onChange={props.handleDropdownParent}>
                     <option value="Choose" selected>Choose</option>
-                    <option value="Chelakkara">Chelakkara</option>
-                    <option value="Kunnamkulam">Kunnamkulam</option>
-                    <option value="Guruvayur">Guruvayur</option>
-                    <option value="Manalur">Manalur</option>
-                    <option value="Wadakkanchery">Wadakkanchery</option>
-                    <option value="Ollur">Ollur</option>
-                    <option value="Thrissur">Thrissur</option>
-                    <option value="Nattika">Nattika</option>
-                    <option value="Kaipamangalam">Kaipamangalam</option>
-                    <option value="Irinjalakuda">Irinjalakuda</option>
-                    <option value="Pudukkad">Pudukkad</option>
                     <option value="Chalakkudy">Chalakkudy</option>
+                    <option value="Chelakkara">Chelakkara</option>
+                    <option value="Guruvayur">Guruvayur</option>
+                    <option value="Irinjalakuda">Irinjalakuda</option>
+                    <option value="Kaipamangalam">Kaipamangalam</option>
                     <option value="Kodungallur">Kodungallur</option>
+                    <option value="Kunnamkulam">Kunnamkulam</option>
+                    <option value="Manalur">Manalur</option>
+                    <option value="Nattika">Nattika</option>
+                    <option value="Ollur">Ollur</option>
+                    <option value="Pudukkad">Pudukkad</option>
+                    <option value="Thrissur">Thrissur</option>
+                    <option value="Wadakkanchery">Wadakkanchery</option>
                 </select>
             </div>
         </div>
