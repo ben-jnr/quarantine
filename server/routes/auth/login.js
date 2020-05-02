603cbfff4973144093049c4f98fabf1efb178ec7
Abhinav KM <abhinavkm184@gmail.com>
2020-05-03 01:55:41 +0530

    login bug fix

--- server/routes/auth/login.js
+++ server/routes/auth/login.js
@@ -32,7 +32,9 @@ module.exports = function(app)
         
         //Route for Login Form
         app.post("/api/login",function(req,res){
+            console.log("Request: Login");
             user.findOne({username:req.body.username},function(err,currUser){
+                console.log("Login: currUser ", currUser, " err ", err);
                 if(currUser)
                 {
                     if(bcrypt.compareSync(req.body.password, currUser.password))
@@ -59,16 +61,20 @@ module.exports = function(app)
                                     res.send(sessions[sessions.length-1]._id);
                                 })
                             })    
-                        }                    
+                        }
+                        else{
+                            console.log("Login: query.id not found");
+                        }                 
                     }
                     else
                     {
-                        console.log("curr user", currUser);
+                        console.log("Login: invalid password, curr user", currUser);
                         res.send('Invalid Credentials');
                     }
                 }
                 else
                 {
+                    console.log("Login: invalid username");
                     res.send('Invalid Credentials');
                 }
             })
