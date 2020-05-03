acf919d5e143a68e9706d352f305bb993c5dc54d
Abhinav KM <abhinavkm184@gmail.com>
2020-05-03 20:49:04 +0530

    exporting data

---
+++ server/routes/count/exportData.js
@@ -0,0 +1,37 @@
+const json2csv = require("json2csv").parse;
+const fs = require("fs");
+
+var MongoPool = require("../db/db");
+MongoPool.getInstance(function (db){
+    const quarantine = db.db("quarantine");
+    const institutions = quarantine.collection("institution");
+
+    institutions.find().toArray( (err, instList)=>{
+        if(err){
+            console.log("DB Fetch error. ", err);
+            return;
+        }
+
+        // Fields: District, Name, Taluk, Constituency, Panchayat, Village, Capacity
+        const exportData = [];
+        var slNo = 1;
+        instList.splice(1, instList.length - 1).map( inst=>{
+            let row = {
+                "Sl No": slNo,
+                "Name": inst.name,
+                "Capacity": inst.rooms.length,
+                "Panchayath": inst.panchayat,
+                "Village": inst.village,
+                "Taluk": inst.taluk,
+                "Constituency": inst.constituency
+            }
+            slNo+=1;
+            exportData.push(row);
+        })
+        const csv = json2csv(exportData);
+        fs.writeFile("report.csv", csv, (err)=>{
+            if(err)
+                console.log("Could not saved");
+        });
+    });
+});
